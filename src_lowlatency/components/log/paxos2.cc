f74f47a818d7637fefa38ffeda3ba5066382c851
kunrenyale <kun.ren@yale.edu>
2016-09-07 14:29:50 -0400

    add debug information

--- src_lowlatency/components/log/paxos2.cc
+++ src_lowlatency/components/log/paxos2.cc
@@ -74,6 +74,7 @@ void Paxos2App::Append(uint64 blockid, uint64 count) {
   header->add_misc_int(blockid);
   header->add_misc_int(count);
   machine()->SendMessage(header, new MessageBuffer());
+LOG(ERROR) << "Paxos2 recevie a Append request. block id is:"<< blockid;
 }
 
 void Paxos2App::Start() {
@@ -106,6 +107,7 @@ void Paxos2App::HandleOtherMessages(Header* header, MessageBuffer* message) {
     p->set_first(header->misc_int(0));
     p->set_second(header->misc_int(1));
     count_ += p->second();
+LOG(ERROR) << "++Paxos2 recevie a Append request. block id is:"<< header->misc_int(0);
 
   } else if (header->rpc() == "NEW-SEQUENCE") { 
     sequences_other_replicas.Push(message);   
@@ -184,6 +186,7 @@ void Paxos2App::RunLeader() {
         
         local_versions_index_table.Put(local_sequences_index, version);
         local_sequences_index++;
+LOG(ERROR) << "Paxos2 proposes a new sequence from local: version:"<< version;
       }
     } else if (sequences_other_replicas.Size() != 0) {
       sequences_other_replicas.Pop(&m);
@@ -196,6 +199,7 @@ void Paxos2App::RunLeader() {
       from_machine = FromScalar<uint32>(s);
 
       isLocal = false;
+LOG(ERROR) << "Paxos2 proposes a new sequence from other replicas: version:"<< version;
     }
 
     atomic<int>* acks = new atomic<int>(1);
@@ -232,6 +236,7 @@ void Paxos2App::RunLeader() {
 
     // Actually append the request into the log
     log_->Append(version, encoded);
+LOG(ERROR) << "Paxos2: Actually append the request into the log: version:"<< version;
 
     if (isLocal == true && isFirst == true) {
       // Send the sequence to the LeaderPaxosApp of all the other replicas;
@@ -250,6 +255,7 @@ void Paxos2App::RunLeader() {
           machine()->SendMessage(header, m);
 
           next_sequences_index.Put(i, 1);
+LOG(ERROR) << "Paxos2: Send NEW-SEQUENCE:";
 	}
       }
 
@@ -265,6 +271,7 @@ void Paxos2App::RunLeader() {
       m = new MessageBuffer();
       m->Append(ToScalar<uint32>(machine()->machine_id()));
       machine()->SendMessage(header, m);
+LOG(ERROR) << "Paxos2: Send NEW-SEQUENCE-ACK:";
     }
 
   }
