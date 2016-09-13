eaca1c3edc05341b5628b49c0e7ef268636236d1
kunrenyale <kun.ren@yale.edu>
2016-09-13 16:01:36 -0400

    test

--- src/fs/calvinfs_client_app.h
+++ src/fs/calvinfs_client_app.h
@@ -98,7 +98,7 @@ class CalvinFSClientApp : public App {
 //    LatencyExperiment();
 //    CrashExperiment();
   }
-  static const int kMaxCapacity = 20;
+  static const int kMaxCapacity = 100;
 
   virtual void HandleMessage(Header* header, MessageBuffer* message) {
     // INTERNAL metadata lookup
