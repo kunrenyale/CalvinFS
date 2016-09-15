15e8ee3f3be13946fab8c2a863b9ae0d7019fc9b
kunrenyale <kun.ren@yale.edu>
2016-09-15 09:45:22 -0400

    test

--- src/components/scheduler/locking_scheduler.h
+++ src/components/scheduler/locking_scheduler.h
@@ -17,7 +17,7 @@ using std::atomic;
 class LockingScheduler : public Scheduler {
  private:
   static const int kMaxActiveActions = 1000;
-  static const int kMaxRunningActions = 300;
+  static const int kMaxRunningActions = 200;
 
  public:
   LockingScheduler()
