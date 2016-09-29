e0a8aa6baeb06d9c8433035719e01e4256db7198
kunrenyale <kun.ren@yale.edu>
2016-09-29 10:52:26 -0400

    test

--- src_lowlatency/components/scheduler/locking_scheduler.h
+++ src_lowlatency/components/scheduler/locking_scheduler.h
@@ -18,7 +18,7 @@ using std::atomic;
 class LockingScheduler : public Scheduler {
  private:
   static const int kMaxActiveActions = 1000;
-  static const int kMaxRunningActions = 100;
+  static const int kMaxRunningActions = 50;
 
  public:
   LockingScheduler()
