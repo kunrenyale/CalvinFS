9bad0a430015003876bcbaba26b5724698850d19
kunrenyale <kun.ren@yale.edu>
2016-10-04 16:00:13 -0400

    test

--- src_lowlatency/machine/thread_pool/thread_pool.cc
+++ src_lowlatency/machine/thread_pool/thread_pool.cc
@@ -308,7 +308,7 @@ void* ThreadPool::MonitorThread(void* arg) {
       }
       high->Resize_thread_count(high->Thread_count() + add_thread_count);
       high->assigned_thread_count_ += add_thread_count;
-//LOG(ERROR) << ":------------ Need to create more threads, now is: " << high->assigned_thread_count_;
+LOG(ERROR) << ":------------ Need to create more threads, now is: " << high->assigned_thread_count_;
     } else if (high->idle_thread_count_ > high->max_idle_) {
       // Need to delete some threads
       int delete_thread_count = 2;
@@ -323,7 +323,7 @@ void* ThreadPool::MonitorThread(void* arg) {
         high->stopped_.erase(deleted_thread);
       }
       high->Resize_thread_count(high->thread_count_ - delete_thread_count);
-//LOG(ERROR) << ":------------ Need to delete some threads, now is: " << high->thread_count_ - delete_thread_count;
+LOG(ERROR) << ":------------ Need to delete some threads, now is: " << high->thread_count_ - delete_thread_count;
     }
 
     // Check low priority threads
