fb4ce36b4db83557eb72f11a2fb04f422e359523
kunrenyale <kun.ren@yale.edu>
2016-10-04 20:33:56 -0400

    change epoch time to 0.005

--- src/fs/block_log.h
+++ src/fs/block_log.h
@@ -115,7 +115,7 @@ class BlockLogApp : public App {
     going_ = true;
     while (go_.load()) {
       // Create new batch once per epoch.
-      double next_epoch = GetTime() + 0.01;
+      double next_epoch = GetTime() + 0.005;
 
       // Create batch (iff there are any pending requests).
       int count = queue_.Size();
