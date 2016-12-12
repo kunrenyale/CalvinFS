7cdf4fe2a017342e2ebc9270f71c5e4de9d988de
kunrenyale <kun.ren@yale.edu>
2016-12-11 22:33:12 -0500

    test

--- src_lowlatency/fs/block_log.h
+++ src_lowlatency/fs/block_log.h
@@ -511,7 +511,7 @@ class BlockLogApp : public App {
             delete p;
 //LOG(ERROR) << "*********Blocklog subbatch_id:"<< subbatch_id_ << " subbatch_version_:"<<subbatch_version_;
           } else {
-            usleep(100);
+            //usleep(100);
             return false;
           }
         }
