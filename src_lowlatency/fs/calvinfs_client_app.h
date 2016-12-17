459066c3eda884fa205496f134deb63cbf8a3fdf
kunrenyale <kun.ren@yale.edu>
2016-12-17 16:54:57 -0500

    test

--- src_lowlatency/fs/calvinfs_client_app.h
+++ src_lowlatency/fs/calvinfs_client_app.h
@@ -783,7 +783,7 @@ void LatencyExperimentRenameFile(int local_percentage) {
     // Begin mix of operations.
     reporting_ = true;
     double start = GetTime();
-    for (int j = 0; j < 200; j++) {
+    for (int j = 0; j < 250; j++) {
       int seed = rand() % 100;
       
       // Copy operations inside one data center
