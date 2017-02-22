ed71288137a6f388b49a187155d09c6ba740ef9f
kunrenyale <kun.ren@yale.edu>
2017-02-22 10:02:07 -0500

    test

--- src_remaster/fs/calvinfs_client_app.h
+++ src_remaster/fs/calvinfs_client_app.h
@@ -760,7 +760,7 @@ void LatencyExperimentAppend() {
                              "/a" + IntToString(machine()->machine_id()) + "/b" + IntToString(a2) + "/d" + IntToString(machine()->GetGUID()));
       } else {
          // Only test 1 multi-replica rename operation for now
-         if (j == 5) {
+         if (j == 100) {
            BackgroundRenameFile("/a" + IntToString(2) + "/b" + IntToString(1) + "/c" + IntToString(j),
                              "/a" + IntToString(2) + "/b" + IntToString(1) + "/d" + IntToString(machine()->GetGUID()));
          } /**else if (j == 8) {
